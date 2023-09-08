328b661b5f66a2f598a8cc67260b0d4b45c38d16
Luis Ruediger <luisruediger27@gmail.com>
2023-09-08 14:07:25 -0300

    criando app em Storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,3 @@
-import { colors } from '@ignite-ui/tokens'
 import { styled } from './styles'
 
 const Button = styled('button', {
@@ -9,5 +8,5 @@ const Button = styled('button', {
 })
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+  return <Button>Hello World</Button>
 }
