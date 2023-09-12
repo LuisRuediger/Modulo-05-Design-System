444d8a6494b22650840e4a2d26a2fc5e5b162db3
Luis Ruediger <luisruediger27@gmail.com>
2023-09-12 10:18:38 -0300

    component Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/luisruediger.png',
+    alt: 'Luis Ruediger',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
